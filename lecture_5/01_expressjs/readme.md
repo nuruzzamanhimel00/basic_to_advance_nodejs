3a458bbdf8d14e2be07335c9efdcc8aec6324927
Md Nuruzzaman <38957494+nuruzzamanhimel00@users.noreply.github.com>
2026-04-17 00:33:47 +0600

    on express js Book CRUD completed

--- lecture_5/01_expressjs/readme.md
+++ lecture_5/01_expressjs/readme.md
@@ -2,6 +2,6 @@ url: https://expressjs.com/en/starter/installing.html
 
 step 1: make directory for 01_expressjs
 step 2: npm init
-step 3 : npm i @types/node
+step 3 : npm i @types/node -D
 step 4: npm install express
-step 5: npm i @types/express
+step 5: npm i @types/express -D
---
+++ lecture_5/02_expressjs/index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+
+const app = express();
+const port = 8000
+
+const books = [
+    {id: 1 , name: 'Book 1' },
+    {id: 2 , name: 'Book 2' },
+    {id: 3 , name: 'Book 3' },
+]
+
+//install plagins
+app.use(express.json())
+
+app.get('/books', (req, res) => {
+    res.setHeader('x-piy', 'Books API')
+    res.json(books)
+})
+
+app.get('/books/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const book = books.find(b => b.id === id)
+    if (!book) {
+        res.status(404).json({ message: 'Book not found' })
+    }
+    res.json(book)
+})
+
+app.post('/books/store', (req, res) => {
+  // console.log(req.headers)
+  // console.log(req.body)
+    const { name } = req.body
+    const newBook = { id: books.length + 1, name }
+    books.push(newBook)
+    res.status(201).json(books)
+})
+
+app.put('/books/update/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const { name } = req.body
+    const book = books.find(b => b.id === id)
+    if (!book) {
+        res.status(404).json({ message: 'Book not found' })
+    }
+    book.name = name
+    res.json(book)
+});
+
+app.delete('/books/delete/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const bookIndex = books.findIndex(b => b.id === id)
+    if (bookIndex === -1) {
+        res.status(404).json({ message: 'Book not found' })
+    }
+    books.splice(bookIndex, 1)
+    res.json({ message: 'Book deleted successfully' })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
